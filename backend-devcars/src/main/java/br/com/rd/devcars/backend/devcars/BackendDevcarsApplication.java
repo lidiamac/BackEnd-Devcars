a9e833b2d06d70f931db13f87ea7de09965c5116
Augusto Meireles <augustomeireles05@gmail.com>
2022-04-06 10:12:41 -0300

    Criação do projeto Inicial

---
+++ backend-devcars/src/main/java/br/com/rd/devcars/backend/devcars/BackendDevcarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.rd.devcars.backend.devcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDevcarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDevcarsApplication.class, args);
+	}
+
+}
---
+++ backend-devcars/src/test/java/br/com/rd/devcars/backend/devcars/BackendDevcarsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rd.devcars.backend.devcars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDevcarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
