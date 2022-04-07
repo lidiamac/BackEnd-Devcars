bc05e80b1b3d54f30e8334a7a48dd4ae9dccf1e3
Lidia <maciellili@hotmail.com>
2022-04-07 09:30:03 -0300

    entidades

--- spring-devcars/src/main/java/br/com/queroserdev/spring/devcars/orm/Cliente.java
+++ spring-devcars/src/main/java/br/com/queroserdev/spring/devcars/orm/Cliente.java
@@ -1,13 +1,22 @@
 package br.com.queroserdev.spring.devcars.orm;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 @Entity
 @Table(name = "tb_cliente")
@@ -44,13 +53,18 @@ public class Cliente {
 	@Column(name="razao_social", nullable = true)
 	private String razaoSocial;
 	
+	@Fetch(FetchMode.SELECT)
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_endereco_cliente", joinColumns = {
+			@JoinColumn(name = "cod_cliente") }, inverseJoinColumns = { @JoinColumn(name = "cod_endereco") })
+	private List<Endereco> enderecos;
 	
-
+	
+	
 	
 	public Cliente() {
 		
 	}
-	
 	
 	
 	
---
+++ spring-devcars/src/main/java/br/com/queroserdev/spring/devcars/orm/Frete.java
@@ -0,0 +1,93 @@
+package br.com.queroserdev.spring.devcars.orm;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_frete")
+public class Frete {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer cod_frete;
+			
+	@Column(name="unidade_federetiva", nullable = false)
+	private String unidadeFederetiva;
+	
+	@Column(name="valor_frete", nullable = false)
+	private BigDecimal valorFrete;
+	
+	@OneToMany(mappedBy = "frete")
+	private List<Pedido> pedidos;
+	
+	@OneToMany(mappedBy = "frete")
+	private List<CabecalhoNF> cabecalhos;
+
+	
+	
+	
+	public Frete() {
+		super();
+	}
+	
+	
+	
+	
+
+	public Integer getCod_frete() {
+		return cod_frete;
+	}
+
+	public void setCod_frete(Integer cod_frete) {
+		this.cod_frete = cod_frete;
+	}
+
+	public String getUnidadeFederetiva() {
+		return unidadeFederetiva;
+	}
+
+	public void setUnidadeFederetiva(String unidadeFederetiva) {
+		this.unidadeFederetiva = unidadeFederetiva;
+	}
+
+	public BigDecimal getValorFrete() {
+		return valorFrete;
+	}
+
+	public void setValorFrete(BigDecimal valorFrete) {
+		this.valorFrete = valorFrete;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
+	public List<CabecalhoNF> getCabecalhos() {
+		return cabecalhos;
+	}
+
+	public void setCabecalhos(List<CabecalhoNF> cabecalhos) {
+		this.cabecalhos = cabecalhos;
+	}
+
+	
+	
+	@Override
+	public String toString() {
+		return "Frete [cod_frete=" + cod_frete + ", unidadeFederetiva=" + unidadeFederetiva + ", valorFrete="
+				+ valorFrete + ", pedidos=" + pedidos + ", cabecalhos=" + cabecalhos + "]";
+	}
+	
+}
