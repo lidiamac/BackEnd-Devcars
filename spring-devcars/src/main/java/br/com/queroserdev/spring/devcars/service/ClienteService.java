3c042522823c7369f2a019203127ef2b45adbcb2
WThiersLucena <thiers_@hotmail.com>
2022-04-20 09:24:59 -0300

    corrigindo erro local

--- spring-devcars/src/main/java/br/com/queroserdev/spring/devcars/service/ClienteService.java
+++ spring-devcars/src/main/java/br/com/queroserdev/spring/devcars/service/ClienteService.java
@@ -11,55 +11,13 @@ import org.springframework.stereotype.Service;
 import br.com.queroserdev.spring.devcars.orm.Cliente;
 import br.com.queroserdev.spring.devcars.repository.ClienteRepository;
 
-
 @Service
-public class ClienteService {  
-	private final ClienteRepository clienteRepository;
-
-
-
+public class ClienteService {
 	
 	private Boolean continuar = true;
-
 	
     @Autowired
 	ClienteRepository clienteRepository;
-	
-	private void listarCartoes() { // Metodo Listagem de cartões 
-		Iterable<Cliente> clientes = clienteRepository.findAll();
-		clientes.forEach(cliente -> System.out.println(cliente));
-	}
-	                              
-
-	private void dadosPedido() {// Metodo dadosPedido
-		
-	}
-	
-	private void salvarCliente(Scanner sc) { // Método salvarCliente
-		int id;
-		String descricao;
-		
-		System.out.println("Informe o Id do registro a ser atualizado");
-		id = sc.nextInt();
-		
-		System.out.println("Informe a nova descrição para o Cargo");
-		descricao = sc.next();
-		
-		Cliente cliente = new Cliente();
-	//	cliente.setId(id);
-	//	cliente.setDescricao(descricao);
-		
-		clienteRepository.save(cliente);
-		
-		System.out.println("Cargo Atualizado com Sucesso");
-
-	}
-
-	
-	
-	
-	
-	
 	
 	
 	public void iniciar(Scanner sc) throws ParseException {
