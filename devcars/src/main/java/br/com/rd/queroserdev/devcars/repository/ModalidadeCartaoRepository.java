ee544327a1ed86e85ce37fb715ed5a63f3b333b8
Lidia <maciellili@hotmail.com>
2022-05-04 15:31:05 -0300

    restore

--- devcars/src/main/java/br/com/rd/queroserdev/devcars/repository/ModalidadeCartaoRepository.java
+++ devcars/src/main/java/br/com/rd/queroserdev/devcars/repository/ModalidadeCartaoRepository.java
@@ -1,19 +1,19 @@
 package br.com.rd.queroserdev.devcars.repository;
 
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.rd.queroserdev.devcars.model.ModalidadeCartao;
 
 public interface ModalidadeCartaoRepository extends CrudRepository<ModalidadeCartao, Integer> {
 
-//	List<ModalidadeCartao> findByCodModalidadeCartao(Integer codModalidadeCartao);
+	@Query("select m.codModalidadeCartao from ModalidadeCartao m where m.codModalidadeCartao = :codModalidade")
+	Integer findByCodModalidadeCartao( Integer codModalidade);
 
 //	ModalidadeCartao findByDescricaoModalidadeCartao(String modalidadeCartao);
+
 	
-//	ModalidadeCartao findByCodModalidadeCartao(Integer codModalidadeCartao);
 	
-	
-	ModalidadeCartao getById(Integer codModalidadeCartao);
 	
 }
