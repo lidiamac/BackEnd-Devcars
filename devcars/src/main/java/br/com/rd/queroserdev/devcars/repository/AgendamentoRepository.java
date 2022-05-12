5c9c82f48fc33b42f12da08bbfe4c4dd84cde443
Augusto Meireles <augustomeireles05@gmail.com>
2022-05-12 00:01:22 -0300

    Consertando conflito em agendamentoRepository

--- devcars/src/main/java/br/com/rd/queroserdev/devcars/repository/AgendamentoRepository.java
+++ devcars/src/main/java/br/com/rd/queroserdev/devcars/repository/AgendamentoRepository.java
@@ -1,19 +1,15 @@
 package br.com.rd.queroserdev.devcars.repository;
 
-<<<<<<< HEAD
-=======
 import java.util.List;
 
->>>>>>> ff339d2c8564f0b3876a6b62b53649f88e90de4e
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.rd.queroserdev.devcars.model.Agendamento;
 
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Integer>{
-<<<<<<< HEAD
-=======
+
 	
 	List<Agendamento> findByCliente_CodCliente(Integer id);
->>>>>>> ff339d2c8564f0b3876a6b62b53649f88e90de4e
+
 
 }
