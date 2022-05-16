40c11fafc54e7c974345672609f3d5257429a9ce
Guilherme <vianaguilherme29@gmail.com>
2022-05-16 19:59:45 -0300

    enderecos e myOrder

--- devcars/src/main/java/br/com/rd/queroserdev/devcars/repository/EnderecoRepository.java
+++ devcars/src/main/java/br/com/rd/queroserdev/devcars/repository/EnderecoRepository.java
@@ -3,6 +3,8 @@ package br.com.rd.queroserdev.devcars.repository;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,6 +17,8 @@ public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
 	Endereco getIdEndereco(@Param("idEndereco")Integer idEndereco);
 	
 	List<Endereco> findByCodEndereco(Integer codEndereco);
+
+	List<Endereco> findByClientesCodCliente(@Valid Integer id);
 
 
 }
